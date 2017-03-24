1e4783db605a2d18c57fa5b7e421efc2d65425b9
Kate-17 <mezekur@yandex.ru>
2017-03-24 23:39:49 +0800

    Функционал

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,4 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +8,6 @@ import java.util.List;
  */
 public class Menu {
 
-    //todo Создайте свое меню. Должен быть хотя бы один конструктор, который создает объект класса Menu, содержащий список не менее чем из 10 блюд.
-    //todo Должно быть не менее 10 блюд.
-    //todo Список должен быть не пуст
-    //todo Это намеки на создание тестов. Сначала тесты, потом реализация!
 
     /**
      * Возвращает список блюд в меню.
@@ -19,8 +15,25 @@ public class Menu {
      * Должен возвращать одну и ту же коллекцию объектов при кажом вызове.
      * @return список блюд в меню
      */
-    public List<Meal> list(){
-        //todo
-        return Collections.emptyList();
+    static List<Meal> m;
+    public Menu(){
+        m =new ArrayList<>();
+        m.add(new Meal("Soup",4.2f));
+        m.add(new Meal("Soup2",5.2f));
+        m.add(new Meal("Soup3",6.6f));
+        m.add(new Meal("Soup4",7.2f));
+        m.add(new Meal("Soup5",3.3f));
+        m.add(new Meal("Chicken",5.2f));
+        m.add(new Meal("Chicken2",8.6f));
+        m.add(new Meal("Chicken3",9.1f));
+        m.add(new Meal("Chicken4",1.6f));
+        m.add(new Meal("Chicken5",3.0f));
     }
+
+
+    public static List<Meal> list(){
+        return m;
+    }
+
+
 }
--- src/Order.java
+++ src/Order.java
@@ -1,37 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Заказ
  */
 public class Order {
+
+    private List<Meal> order;
+    private List<Integer> amount;
+
+
+    public Order() {
+       order = new ArrayList<>();
+       amount = new ArrayList<>();
+    }
+
     /**
      * Добавляет одно блюдо в заказ.
      *
-     * todo Сначала написать тесты!
-     *
-     *
      * @param meal блюдо из меню
      */
-    public void addMeal(Meal meal){
-        // todo Блюдо должно быть из меню.
-        // todo В случае, если блюдо не изменю, должно кидать IllegalArgumentException
-        // todo Блюдо должно быть не null.
-        // todo добавляем блюдо в заказ
-        throw new UnsupportedOperationException();
+    public void addMeal(Meal meal) {
+        if (Menu.list().contains(meal)) {
+            if (meal != null)
+                if (!order.contains(meal)) {
+                    order.add(meal);
+                    amount.add(1);
+                } else amount.set(order.indexOf(meal), amount.get(order.indexOf(meal)) + 1);
+        }
+        else throw new IllegalArgumentException();
     }
 
-    //todo добавить возможность добавления нескольких порций  одного блюда, например, два чая.
-    //todo чтобы можно было сделать, например, addMeal(teaMeal, 2)
-    //todo TESTS!
-
-
-    /**
-     * Возвращает сумму заказа, суммирующая цены каждого блюда,
-     * умноженные на количество каждого блюда в заказе.
-     *
-     * @return сумму заказа в у.е., число с плавающей точкой
-     */
-    public Float totalSum(){
-        //todo
-        throw new UnsupportedOperationException();
+    public void addMeal(Meal meal, int number) {
+        if (Menu.list().contains(meal)) {
+            if (meal != null)
+                if (!order.contains(meal)) {
+                    order.add(meal);
+                    amount.add(number);
+                } else amount.set(order.indexOf(meal), amount.get(order.indexOf(meal)) + number);
+        }
+        else throw new IllegalArgumentException();
     }
+        /**
+         * Возвращает сумму заказа, суммирующая цены каждого блюда,
+         * умноженные на количество каждого блюда в заказе.
+         *
+         * @return сумму заказа в у.е., число с плавающей точкой
+         */
+
+        public Float totalSum() {
+            Float sum=0f;
+            for (int i=0; i<order.size();i++)
+                sum=sum+order.get(i).getPrice()*amount.get(i);
+            return sum;
+        }
+
 }
