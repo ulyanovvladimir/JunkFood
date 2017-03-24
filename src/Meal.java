43b334835cf4c2b4e74cc3655be8b2d9f7eac5cc
elenkamir <lenamir96@rambler.ru>
2017-03-24 23:09:17 +0900

    Fun

--- src/Meal.java
+++ src/Meal.java
@@ -1,27 +1,28 @@
-/**
- *
- * Блюдо из меню
- */
 public class Meal {
 
-    //todo Добавьте необходимые конструктуры и/или методы, чтобы можно было создать блюдо с указанием его наименования
-    //todo Подумайте, что лучше подходит. Считается, что у блюда название не изменяется ;)
-
-    /**
-     *
-     * @return наименование блюда
-     */
-    public String getTitle(){
-        //todo
-        throw new UnsupportedOperationException();
+    public  static String title;
+    Float price;
+    public  Meal(String title, Float price){
+        this.title = title;
+        this.price = price;
     }
 
-    /**
-     * Вовзвращает цену с плавающей точкой.
-     * @return Цена, число с плавающей точкой в у.е.
-     */
+    public String getTitle(){
+        return this.title;
+
+    }
+
+
     public Float getPrice(){
-        //todo
-        throw new UnsupportedOperationException();
+        return this.price;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Meal){
+            Meal m = (Meal) obj;
+            return m.getTitle().equals(this.getTitle());
+        }else {return false;}
     }
 }
+
--- src/Order.java
+++ src/Order.java
@@ -1,37 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Заказ
- */
 public class Order {
-    /**
-     * Добавляет одно блюдо в заказ.
-     *
-     * todo Сначала написать тесты!
-     *
-     *
-     * @param meal блюдо из меню
-     */
-    public void addMeal(Meal meal){
-        // todo Блюдо должно быть из меню.
-        // todo В случае, если блюдо не изменю, должно кидать IllegalArgumentException
-        // todo Блюдо должно быть не null.
-        // todo добавляем блюдо в заказ
-        throw new UnsupportedOperationException();
+    public static List<Meal> NewOrder;
+    public Order(){
+        NewOrder = new ArrayList<>();
     }
 
-    //todo добавить возможность добавления нескольких порций  одного блюда, например, два чая.
-    //todo чтобы можно было сделать, например, addMeal(teaMeal, 2)
-    //todo TESTS!
+    public void addMeal(Meal meal, int number){
+        if (Menu.containsMeal(meal)){
+            if (meal != null) {
+                for (int i = 0; i < number; i++) {
+                    NewOrder.add(meal);
+                }
+            }
+        }
+        else throw new IllegalArgumentException();
 
+    }
 
-    /**
-     * Возвращает сумму заказа, суммирующая цены каждого блюда,
-     * умноженные на количество каждого блюда в заказе.
-     *
-     * @return сумму заказа в у.е., число с плавающей точкой
-     */
     public Float totalSum(){
-        //todo
-        throw new UnsupportedOperationException();
+        Float total = 0.0F;
+        for(Meal m : NewOrder){
+            total = total + m.getPrice();
+        }
+        return total;
     }
 }
