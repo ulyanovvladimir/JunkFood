fe7fea85ac47bf3803a76e64f534e6a96ff9346a
Mochalova Julia <juliarychkova1@gmail.com>
2017-03-25 20:32:54 +0800

    С реализацией классов

--- src/Meal.java
+++ src/Meal.java
@@ -4,35 +4,39 @@
  */
 public class Meal {
 
-    public Meal (String name, Float cost) {
-        throw new UnsupportedOperationException();
+    String name;
+    Float cost;
+    public Meal(String name, Float cost) {
+        this.name = name;
+        this.cost = cost;
     }
-    //todo Добавьте необходимые конструктуры и/или методы, чтобы можно было создать блюдо с указанием его наименования
-    //todo Подумайте, что лучше подходит. Считается, что у блюда название не изменяется ;)
+
+
+//todo Добавьте необходимые конструктуры и/или методы, чтобы можно было создать блюдо с указанием его наименования
+//todo Подумайте, что лучше подходит. Считается, что у блюда название не изменяется ;)
 
     /**
      *
      * @return наименование блюда
      */
     public String getTitle(){
-        //todo
-        throw new UnsupportedOperationException();
+        return this.name;
     }
 
     /**
      * Вовзвращает цену с плавающей точкой.
      * @return Цена, число с плавающей точкой в у.е.
      */
-    public Float getPrice(){
-        //todo
-        throw new UnsupportedOperationException();
-    }
 
+    public Float getPrice(){
+        return this.cost;
+    }
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Meal){
-            Meal m = (Meal) obj;
+            Meal m =(Meal) obj;
             return m.getTitle().equals(this.getTitle());
-        } else return false;
+        }
+        else return false;
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Заказ
@@ -6,26 +8,41 @@ public class Order {
     /**
      * Добавляет одно блюдо в заказ.
      *
+     *
      * todo Сначала написать тесты!
      *
      *
      * @param meal блюдо из меню
      */
+
+    private static List<Meal> lorder;
+
+    public Order(){
+        lorder= new ArrayList<>();
+    }
     public void addMeal(Meal meal){
         // todo Блюдо должно быть из меню.
         // todo В случае, если блюдо не изменю, должно кидать IllegalArgumentException
         // todo Блюдо должно быть не null.
-        // todo добавляем блюдо в заказ
-        throw new UnsupportedOperationException();
+        if (!(meal==null) && Menu.lcontains(meal)){
+            // todo добавляем блюдо в заказ
+            lorder.add(meal);
+        }
+        else throw new IllegalArgumentException();
     }
-
     public void addMeal(Meal meal,int count){
         // todo Блюдо должно быть из меню.
         // todo В случае, если блюдо не изменю, должно кидать IllegalArgumentException
         // todo Блюдо должно быть не null.
-        // todo добавляем блюдо в заказ
-        throw new UnsupportedOperationException();
+        if (meal!=null && Menu.lcontains(meal)){
+            // todo добавляем блюдо в заказ
+            for (int i=0; i<count;i++) {
+                lorder.add(meal);
+            }
+        }
+        else throw new IllegalArgumentException();
     }
+
     //todo добавить возможность добавления нескольких порций  одного блюда, например, два чая.
     //todo чтобы можно было сделать, например, addMeal(teaMeal, 2)
     //todo TESTS!
@@ -38,7 +55,10 @@ public class Order {
      * @return сумму заказа в у.е., число с плавающей точкой
      */
     public Float totalSum(){
-        //todo
-        throw new UnsupportedOperationException();
+        Float sum = (float) 0;
+        for (Meal e : lorder){
+            sum=sum+ e.getPrice();
+        }
+        return sum;
     }
 }
