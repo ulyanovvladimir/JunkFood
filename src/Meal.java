e24ea7e7262ad95e827c11df1e5547d9888ae31d
NastyaKuku <nastya_rij@mail.ru>
2017-04-27 07:25:21 +0800

    Добавили реализацию

--- src/Meal.java
+++ src/Meal.java
@@ -6,30 +6,50 @@ public class Meal {
 
     //todo Добавьте необходимые конструктуры и/или методы, чтобы можно было создать блюдо с указанием его наименования
     //todo Подумайте, что лучше подходит. Считается, что у блюда название не изменяется ;)
+    private final String title;
+    private Float price;
 
     /**
-     *
      * @return наименование блюда
      */
-    public String getTitle(){
-        //todo
-        throw new UnsupportedOperationException();
+    public Meal(String title, Float price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return this.title;
+        //        throw new UnsupportedOperationException();
     }
 
     /**
      * Вовзвращает цену с плавающей точкой.
+     *
      * @return Цена, число с плавающей точкой в у.е.
      */
-    public Float getPrice(){
-        //todo
-        throw new UnsupportedOperationException();
+
+    public Float getPrice() {
+        return this.price;
+        //  throw new UnsupportedOperationException();
     }
+
+    public void setPrice(Float newPrice) {
+        this.price = newPrice;
+    }
+
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Meal){
+        if (obj instanceof Meal) {
             Meal m = (Meal) obj;
             return m.getTitle().equals(this.getTitle());
-        } else return false;
+        } else {
+            return false;
+        }
     }
+
+    public int hashCode() {
+        return this.title.hashCode();
+    }
+
 }
